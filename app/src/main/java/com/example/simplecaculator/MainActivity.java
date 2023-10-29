cb560a72572c880a96ba19dd7fcaf66008a1ec5b
rinnit1234 <148884502+rinnit1234@users.noreply.github.com>
2023-10-29 20:17:42 +0200

    final touches2

--- app/src/main/java/com/example/simplecaculator/MainActivity.java
+++ app/src/main/java/com/example/simplecaculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,19 +24,26 @@ public class MainActivity extends AppCompatActivity {
         String et2Text = et2.getText().toString();
         Integer num2 = Integer.valueOf(et2Text);
         Integer result = null;
+        boolean error=false;
         if (view.getId() == R.id.btnPlus)
             result= num1+num2;
         if (view.getId()==R.id.btnMinus)
             result=num1-num2;
         if (view.getId()==R.id.btnMult)
             result=num1*num2;
-        if (view.getId()==R.id.btnDiv)
-            result=num1/num2;
-        if (result!=null) {
-            TextView tvRes = findViewById(R.id.tvResult);
-            tvRes.setText(result.toString());
+        if (view.getId()==R.id.btnDiv) {
+            if (num2 == 0) {
+                Toast.makeText(this, "Can't divide by 0", Toast.LENGTH_LONG).show();
+                error = true;
+            }
+            result = num1 / num2;
         }
-
+        if (error==false) {
+            if (result != null) {
+                TextView tvRes = findViewById(R.id.tvResult);
+                tvRes.setText(result.toString());
+            }
+        }
 
 
 
