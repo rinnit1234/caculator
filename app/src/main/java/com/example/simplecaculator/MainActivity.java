d23e73bae53a4fe3c665fef57a649bab127a764a
rinnit1234 <148884502+rinnit1234@users.noreply.github.com>
2023-10-29 20:06:23 +0200

    added code to onclick

--- app/src/main/java/com/example/simplecaculator/MainActivity.java
+++ app/src/main/java/com/example/simplecaculator/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onBtnClicked(View view) {
+        EditText et1 = findViewById(R.id.Numet1);
+        String et1Text = et1.getText().toString();
+        Integer num1 = Integer.valueOf(et1Text);
+        EditText et2 = findViewById(R.id.Numet2);
+        String et2Text = et2.getText().toString();
+        Integer num2 = Integer.valueOf(et2Text);
+        Integer result = null;
+        if (view.getId() == R.id.btnPlus)
+            result= num1+num2;
+        if (view.getId()==R.id.btnMinus)
+            result=num1-num2;
+        if (view.getId()==R.id.btnMult)
+            result=num1*num2;
+        if (view.getId()==R.id.btnDiv)
+            result=num1/num2;
+        if (result!=null) {
+            TextView tvRes = findViewById(R.id.tvResult);
+            tvRes.setText(result.toString());
+        }
+
+
+
+
 
     }
 }
